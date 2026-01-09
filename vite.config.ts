e82da345d69a926021758c93dd430567de1682d7
Qsd6917 <3355243615@qq.com>
2026-01-09 18:35:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  // Use '.' instead of process.cwd() to avoid type issues if process is not fully typed in some contexts.
   const env = loadEnv(mode, '.', '');
   
   return {
+    base: '/DLUT-GPA/', // <--- 核心修改：确保静态资源路径指向你的仓库名
     plugins: [react()],
     define: {
       // Polyfill process.env.API_KEY for the client-side code
-      // It will replace `process.env.API_KEY` with the string value of VITE_API_KEY during build
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY),
     },
   };
